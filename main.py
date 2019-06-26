1b80c1fa84677295eb4009571b82c0b166e670f1
njschafi <njschafi@gmail.com>
2019-06-26 09:44:06 -0700

    flask-mailroom assignment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,9 +15,40 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.jinja2')
+
+    if request.method == 'POST':
+        try:
+            donor = Donor.get(Donor.name == request.form['name'])
+            new_donation = Donation(value=int(request.form['donation']),
+                                    donor=donor)
+            new_donation.save()
+            return redirect(url_for('all'))
+        except Donor.DoesNotExist:
+            print('no donor found')
+            return render_template('create.jinja2',
+                                   error="!!!ERROR - Donor not in database!!!")
+
+@app.route('/view')
+def view():
+    name = request.args.get('name', None)
+
+    if name is None:
+        return render_template('view.jinja2')
+
+    try:
+        donor = Donor.get(Donor.name == name)
+        donations = Donation.select().where(Donation.donor == donor)
+        return render_template('view.jinja2', donations=donations,
+                               donor_name=donor.name)
+    except Donor.DoesNotExist:
+        return render_template('view.jinja2',
+                               error="!!!ERROR - Donor not in database!!!")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
